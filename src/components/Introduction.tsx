a9112d704e7da1c9a2b1997b509a6c04febcca29
dongmin7208 <sayg3o3d@gmail.com>
2024-11-19 10:43:47 +0900

    FIX: Introduction css

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -2,55 +2,57 @@ import { motion } from "framer-motion";
 
 export default function Introduction() {
   return (
-    <section id="introduction" className="mx-2 my-40 text-center">
+    <section
+      id="introduction"
+      className="flex flex-col items-center mx-2 my-40 text-center"
+    >
       {/* 제목 */}
-      <motion.h2
-        className="text-5xl font-bold mb-6 bg-customBg text-blue-600"
+      <motion.div
+        className="bg-customBg px-4 py-2 rounded-lg mb-6"
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="block md:inline">Tokyo IT</span>{" "}
-        <span className="block md:inline">Group</span>
-      </motion.h2>
+        <h2 className="text-5xl font-bold text-blue-600">Tokyo IT Group</h2>
+      </motion.div>
 
       {/* 첫 번째 문장 */}
-      <motion.p
-        className="text-xl mt-12 bg-customBg text-gray-700"
+      <motion.div
+        className="bg-customBg px-4 py-2 rounded-lg mb-4"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <span className="block md:inline">一人では挫けそうなときも、</span>{" "}
-        <span className="block md:inline">仲間がいれば乗り越えられる。</span>
-      </motion.p>
+        <p className="text-xl text-gray-700">
+          一人では挫けそうなときも、仲間がいれば乗り越えられる。
+        </p>
+      </motion.div>
 
       {/* 두 번째 문장 */}
-      <motion.p
-        className="text-xl mb-12 bg-customBg text-gray-700"
+      <motion.div
+        className="bg-customBg px-4 py-2 rounded-lg mb-4"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <span className="block md:inline">私たちは、共に学び合い、</span>{" "}
-        <span className="block md:inline">
-          成長し続けるエンジニアの集まりです。
-        </span>
-      </motion.p>
+        <p className="text-xl text-gray-700">
+          私たちは、共に学び合い、成長し続けるエンジニアの集まりです。
+        </p>
+      </motion.div>
 
       {/* 세 번째 문장 */}
-      <motion.p
-        className="text-lg mb-12 bg-customBg text-gray-600"
+      <motion.div
+        className="bg-customBg px-4 py-2 rounded-lg mb-12"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <span className="inline-block">
+        <p className="text-lg text-gray-600">
           新しい技術の探求、自分を磨く努力、そして仲間との絆がここにはあります。
-        </span>
-      </motion.p>
+        </p>
+      </motion.div>
 
       {/* 이미지 */}
       <motion.img
         src="/images/1.WEBP"
         alt="メイン画像"
-        className="mx-auto rounded-lg shadow-lg w-full max-w-4xl"
+        className="rounded-lg shadow-lg w-full max-w-4xl"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       />
